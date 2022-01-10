f18b9849a248a7b10b24423c05cbd760b0109463
REBEL725 <81420701+REBEL725@users.noreply.github.com>
2022-01-10 16:16:37 +0530

    Update __main__.py

--- RiZoeLXSpam/__main__.py
+++ RiZoeLXSpam/__main__.py
@@ -22,7 +22,7 @@ for name in files:
         plugin_name = patt.stem
         load_plugins(plugin_name.replace(".py", ""))
 
-print("RiZoeL Bot Spam Successfully deployed -!")
+print("geyzole , lundzole  Bot Spam Successfully deployed -!")
 print("Enjoy! Do visit @RiZoeLX")
 
 if len(argv) not in (1, 3, 4):
