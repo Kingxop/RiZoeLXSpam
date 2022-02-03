5e91c77d93434739b68a5b22356fd87e0f8711e4
Kingxop <95347735+Kingxop@users.noreply.github.com>
2022-02-03 13:44:21 +0530

    Update join.py

--- RiZoeLXSpam/plugins/join.py
+++ RiZoeLXSpam/plugins/join.py
@@ -40,7 +40,7 @@ async def _(e):
             event = await e.reply(text, parse_mode=None, link_preview=None )
             try:
                 await e.client(functions.channels.JoinChannelRequest(channel=bc))
-                await event.edit("𝐉𝐨𝐢𝐧 𝐇𝐨𝐠𝐲𝐚 𝐒𝐢𝐫")
+                await event.edit("𝘑𝘖𝘐𝘕 𝘋𝘖𝘕𝘌 𝘉𝘈𝘉𝘠")
             except Exception as e:
                 await event.edit(str(e))   
         else:
